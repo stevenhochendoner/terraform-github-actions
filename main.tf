f8bb6c6fd892a61efb74a19c7bc24fce1ebd7634
stevenhochendoner <125991854+stevenhochendoner@users.noreply.github.com>
2023-03-03 14:32:04 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -26,3 +26,26 @@ resource "azurerm_resource_group" "steve-test-rg" {
   name     = var.resource_group_name
   location = var.location
 }
+
+resource "azurerm_windows_virtual_machine" "windowsvm" {
+  name                  = var.windows_virtual_machine_name
+  location              = var.location
+  resource_group_name   = azurerm_resource_group.name
+  network_interface_ids = [azurerm_network_interface.nic.id]
+  size                  = var.vm_size
+  admin_username        = "spadmin"
+  admin_password        = "$phere2023!"
+  computer_name         = var.vm_hostname
+
+  os_disk  {
+    name                 = "${var.windows_virtual_machine_name}_os_disk"
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "MicrosoftWindowsServer"
+    offer     = "WindowsServer"
+    sku       = "2022-Datacenter-Azure-Edition"
+    version   = "latest"
+  }
